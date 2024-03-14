45914f7cb2f355c8e1d4763261ee97c7a36dbc9a
Przemek B <pprzemek89@gmail.com>
2024-03-14 13:22:53 +0100

    rel uuu bratku

---
+++ src/Krystian.java
@@ -0,0 +1,11 @@
+public class Krystian implements Uczen{
+
+    @Override
+    public void uczSie() {
+        System.out.println("AE");
+    }
+
+    public void ogladajKiepskich(){
+        System.out.println("ĄĄĄĄĄĄĄĄĄĄ");
+    }
+}
---
+++ src/Roch.java
@@ -0,0 +1,11 @@
+public class Roch implements Uczen{
+
+    @Override
+    public void uczSie() {
+        System.out.println("Karaluch");
+    }
+
+    public void GrajWSzachy(){
+        System.out.println("ETV4U");
+    }
+}
