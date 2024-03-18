060a543ec9c26c5bdac7ef6456b3961e57160b00
Student <denys.yermiichuk@teb.edu.pl>
2024-03-18 12:59:17 +0100

    uuu frytma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
+        Krystian krystian = new Krystian();
+        Roch roch = new Roch();
 
+        ArrayList<Uczen> uczniowie = new ArrayList<>();
+
+        uczniowie.add(krystian);
+        uczniowie.add(roch);
+
+        Collections.sort(uczniowie);
+
+
+        for (Uczen u: uczniowie){
+            System.out.println(u.getClass().getName());
+            u.uczSie();
         }
+
     }
+}
--- src/Uczen.java
+++ src/Uczen.java
@@ -1,4 +1,11 @@
-public interface Uczen {
+public interface Uczen extends Comparable<Uczen>{
 
     void uczSie();
+
+    @Override
+    default int compareTo(Uczen o) {
+        String nazwa = getClass().getName();
+        String nazwaDruga = o.getClass().getName();
+        return nazwa.length() - nazwaDruga.length();
+    }
 }
